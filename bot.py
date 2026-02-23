a564a7b943931e97e53fce879249a240de2c3fbe
gha06331-glitch <gha06331@gmail.com>
2026-02-23 23:24:04 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,26 @@
 import os
+import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
-from googletrans import Translator
 
-# گرفتن توکن از Environment Variable
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
-    raise ValueError("توکن ربات پیدا نشد! حتما BOT_TOKEN در محیط تعریف شده باشد.")
-
-translator = Translator()
+    raise ValueError("توکن ربات پیدا نشد! BOT_TOKEN را در تنظیمات Render اضافه کن.")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! متن انگلیسی رو بفرست تا ترجمه کنم به فارسی.")
 
+def translate_text(text):
+    url = "https://api.mymemory.translated.net/get"
+    params = {"q": text, "langpair": "en|fa"}
+    response = requests.get(url, params=params).json()
+    return response["responseData"]["translatedText"]
+
 async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
-    translated = translator.translate(text, dest='fa')
-    await update.message.reply_text(translated.text)
+    translated = translate_text(text)
+    await update.message.reply_text(translated)
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
